8c9892c6ae4b46871045c372a098a0ade3b3a4ce
AnastasiiaRozlyvan <48979187+AnastasiiaRozlyvan@users.noreply.github.com>
2020-06-30 11:46:34 +0300

    [CCTRI-1254] Implement refer/observables (#6)

--- config.py
+++ config.py
@@ -9,6 +9,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
     API_URL = 'https://signals.api.auth0.com/v2.0/'
+    UI_URL = 'https://auth0.com/signals/ip/{value}-report'
 
     USER_AGENT = ('Cisco Threat Response Integrations '
                   '<tr-integrations-support@cisco.com>')
